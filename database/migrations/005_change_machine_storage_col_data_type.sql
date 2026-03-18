cf46179cbfc10e00d15c4a6dee4872dfd6f6ca1d
Francis Ge <francisge527@gmail.com>
2026-03-18 21:22:27 +0800

    add migration file for altering machine storage table col data type from numeric to int

---
+++ database/migrations/005_change_machine_storage_col_data_type.sql
@@ -0,0 +1,11 @@
+ALTER TABLE machine_storage
+ALTER COLUMN one TYPE INT;
+
+ALTER TABLE machine_storage
+ALTER COLUMN five TYPE INT;
+
+ALTER TABLE machine_storage
+ALTER COLUMN ten TYPE INT;
+
+ALTER TABLE machine_storage
+ALTER COLUMN twenty TYPE INT;
