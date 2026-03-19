e5514df2a928f432c2b5f4f86c3c535400de1c58
Francis Ge <francisge527@gmail.com>
2026-03-19 21:52:30 +0800

    delete migration files to start over

--- database/migrations/001_create_admin_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE admins (
-	admin_id SERIAL PRIMARY KEY,
-	email VARCHAR(255) UNIQUE NOT NULL,
-	hashed_password TEXT NOT NULL,
-	account_name VARCHAR(25) NOT NULL
-);
--- database/migrations/003_add_refill_date_col.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE machine_storage
-ADD refill_date_time TIMESTAMP DEFAULT NOW();
