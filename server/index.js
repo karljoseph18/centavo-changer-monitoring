6db669f938ba29b8d52a0c0a9b70c4a1a64f46c7
Francis Ge <francisge527@gmail.com>
2026-03-24 21:46:14 +0800

    enable cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import transactionsRoutes from "./routes/transactions.route.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 3000;
 
 // middlewares
 app.use(express.json());
+app.use(cors());
 
 // routes
 app.use("/api", transactionsRoutes);
